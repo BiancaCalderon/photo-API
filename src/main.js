0b2b5f1e0159140bb98a1d9f4cee2b84570820f3
SergioAle210 <ore221122@uvg.edu.gt>
2024-05-02 00:42:29 +0000

    fix: token function as get

--- src/main.js
+++ src/main.js
@@ -126,7 +126,7 @@ app.post('/admin/login', async (req, res) => {
 });
 
 // Verificar el token
-app.post('/admin/verify-token', async (req, res) => {
+app.get('/admin/verify-token', async (req, res) => {
   const token = req.headers.authorization?.split(' ')[1]; 
   if (!token) {
     return res.status(401).json({ success: false, message: 'Token not provided' });
